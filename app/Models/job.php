8daaf995741e8bd6b0565bd6d294cbbc2439488b
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-09-24 15:28:27 +0530

    Generating fake data for the job model
    -Create migration and factoories,
    -In the migration files create all the necassary columns in the table
    -create factory and speciify which fake data should be generated
    -seed the data in the database seeder files
    and run the miration, using the php artisan migrate:refresh --seed (to specify the seeding of the database )

--- app/Models/job.php
+++ app/Models/job.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class job extends Model
 {
     use HasFactory;
+    public static array $experiences = ['entry', 'intermediate', 'senior'];
+    public static array $jobCategories = [
+        'IT', 'Finance', 'Marketing', 'Sales', 'Design', 'Engineering', 'Legal', 'Accounting', 'Other'
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\job;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        job::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
