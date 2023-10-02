ce76f780cf430a41352563c287b249117e3eacbe
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-10-02 12:48:05 +0530

    Refactoring Filtering Backend Logic

--- app/Models/job.php
+++ app/Models/job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class job extends Model
 {
@@ -12,4 +14,22 @@ class job extends Model
     public static array $jobCategories = [
         'IT', 'Finance', 'Marketing', 'Sales', 'Design', 'Engineering', 'Legal', 'Accounting', 'Other'
     ];
+
+    public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
+    {
+        return $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search  . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
+            });
+        })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
+            $query->where('salary', '>=', $minSalary);
+        })->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {
+            $query->where('salary', '<=', $maxSalary);
+        })->when($filters['experience'] ?? null, function ($query, $experience) {
+            $query->where('experience', 'like',  $experience);
+        })->when($filters['category'] ?? null, function ($query, $jobCategorie) {
+            $query->where('category', 'like',  $jobCategorie);
+        });
+    }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="mx-auto mt-10 max-w-2xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 text-slate-700">
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-sky-500 to-indigo-500 after:text-slate-700">
 
     {{ $slot }}
 
