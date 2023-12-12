597c21993fe7ca092def0b4d64522227e45e4feb
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-12-13 00:28:32 +0530

    Creating the login form
    manipulating tailwing config to set default classes and creating the form layout

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,5 +1,31 @@
 <x-layout>
-    <x-card>
-        login!
+    <h1 class="my-10 text-center text-5xl font-medium text-slate-100 capitalize ">Sign in to your account</h1>
+    <x-card class="py-8 px-16">
+        <form action="{{ route('auth.store') }}" method="POST">
+            @csrf
+
+            <div class="mb-8">
+                <label for="E-mail" class="mb-2 block text-xl font-medium text-slate-900">E-mail</label>
+                <x-text-input name="email" type="email" />
+            </div>
+
+            <div class="mb-8">
+                <label for="password" class="mb-2 block text-xl font-medium text-slate-900">Password</label>
+                <x-text-input name="password" type="password" />
+            </div>
+
+            <div class="mb-8 flex justify-between">
+                <div>
+                    <div class="flex space-x-2 items-center justify-center">
+                        <input type="checkbox" name="remember"
+                            class="rounded border-slate-300 text-indigo-600 shadow-sm">
+                        <label for="checkbox">Remember Me</label>
+                    </div>
+                </div>
+                <div><a href="#" class="text-indigo-600 hover:underline">Reset the password</a></div>
+            </div>
+
+            <x-button type="submit" class="w-full text-base capitalize">Sign in</x-button>
+        </form>
     </x-card>
 </x-layout>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -8,7 +8,7 @@
             </svg>
         </button>
     @endif
-    <input x-ref="input-{{ $name }}" type="text" name="{{ $name }}" placeholder="{{ $placeholder }}"
+    <input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}"
         value="{{ $value }}" id="{{ $name }}"
         class="w-full rounded-md border-0 py-1.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 fucus:ring-1 pr-8" />
 </div>
