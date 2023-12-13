962a4f7603520c91de4088de9c80036f17a80412
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-12-13 23:17:12 +0530

    Working on the authentication sign in functionality

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,9 +35,9 @@ class DatabaseSeeder extends Seeder
         // job::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,8 +8,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-sky-500 to-indigo-500 after:text-slate-700">
+<body class="mx-auto px-8 mt-10 max-w-2xl bg-gradient-to-r from-sky-500 to-indigo-500 after:text-slate-700">
+    {{ auth()->user()->name ?? 'login as Guest' }}
 
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
     {{ $slot }}
 
 </body>
