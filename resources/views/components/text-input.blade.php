cbe95297bfa5b5f5c514594fb2d2b144e8f7096a
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2024-03-31 02:14:21 +0530

    Refactoring
    -Adding validation errors under the input fields
    -Add textarea input field

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,14 +1,33 @@
 <div class="relative">
-    @if ($formRef)
-        <button type="button"
-            class="flex absolute right-0 top-0 h-full items-center pr-2" @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-4 h-4 text-slate-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
+    @if ('textarea' != $type)
+        @if ($formRef)
+            <button type="button" class="flex absolute right-0 top-0 h-full items-center pr-2"
+                @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-4 h-4 text-slate-500">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+        @endif
+        <input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}"
+            placeholder="{{ $placeholder }}" value="{{ $value }}" id="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-1.5 px-2.5 ring-1 placeholder:text-slate-400 fucus:ring-1',
+                'pr-8' => $formRef,
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ]) />
+    @else
+        <textarea id="{{ $name }}" name="{{ $name }}" @class([
+            'w-full rounded-md border-0 py-1.5 px-2.5 ring-1 placeholder:text-slate-400 fucus:ring-1',
+            'pr-8' => $formRef,
+            'ring-slate-300' => !$errors->has($name),
+            'ring-red-300' => $errors->has($name),
+        ])>{{ old($name, $value) }}</textarea>
     @endif
-    <input x-ref="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}"
-        value="{{ $value }}" id="{{ $name }}"
-        class="w-full rounded-md border-0 py-1.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 fucus:ring-1 pr-8" />
+    @error($name)
+        <div class="mt-1 test-sm text-red-500">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
